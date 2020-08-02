6a4234f2fc0c27661700df1c7f663508c7b6176f
UVCoder1002 <uv6coder@gmail.com>
2020-08-02 22:47:07 +0530

    UI OF CHAT PAGE

---
+++ app.js
@@ -0,0 +1,11 @@
+const messageTypes = { LEFT: "left", RIGHT: "right", LOGIN: login };
+
+//CHAT STUFF
+
+const chatwindow = document.getElementById("chat");
+const messageList = document.getElementById("message-list");
+const loginBtn = document.getElementById("loginBtn");
+
+//LOGIN STUFF
+
+const messages = []; //AUTHOR,DATE,CONTENT,TYPE
